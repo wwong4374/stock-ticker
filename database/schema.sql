91b9da06709eb43f752699f65e7c458d66a3628b
Wilson Wong <wilson.ka.wong@gmail.com>
2021-12-04 15:48:11 -0600

    Commit before rewriting stocks table to include stock price

--- database/schema.sql
+++ database/schema.sql
@@ -7,17 +7,18 @@ USE stockPortfolio;
 CREATE TABLE stocks (
   id INT PRIMARY KEY AUTO_INCREMENT,
   stockSymbol VARCHAR(10),
-  quantity INT
+  quantity INT,
+  price FLOAT
 );
 
 INSERT INTO stocks (id, stockSymbol, quantity)
-VALUES (DEFAULT, 'TSLA', 500);
+VALUES (DEFAULT, 'TSLA', 500, 1010.10);
 
 INSERT INTO stocks (id, stockSymbol, quantity)
-VALUES (DEFAULT, 'AAPL', 100);
+VALUES (DEFAULT, 'AAPL', 100, 161.84);
 
 INSERT INTO stocks (id, stockSymbol, quantity)
-VALUES (DEFAULT, 'GM', 200);
+VALUES (DEFAULT, 'GM', 200, 59.71);
 
 INSERT INTO stocks (id, stockSymbol, quantity)
-VALUES (DEFAULT, 'V', 1000);
+VALUES (DEFAULT, 'V', 1000, 196.32);
