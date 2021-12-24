aa8e8b6b00f2032598534da9e062cd81c19fbe9c
Wilson Wong <wilson.ka.wong@gmail.com>
2021-12-24 13:42:04 -0600

    Implemented the getSymbols and getPortfolio queries on the backend

--- server/index.js
+++ server/index.js
@@ -39,16 +39,13 @@ app.get('/api/portfolio/symbols', (req, res) => {
 // Render portfolio
 app.get('/api/portfolio', (req, res) => {
   db.query(
-    `SELECT JSON_ARRAYAGG(
-      JSON_OBJECT(
-        'symbol', a.symbol,
-        'quantity', (SELECT SUM(b.quantity) FROM trades AS b WHERE b.symbol=a.symbol)
-      )
-    ) FROM trades AS a`,
+    `SELECT DISTINCT
+      symbol,
+      (SELECT SUM(quantity) FROM trades AS b WHERE b.symbol=a.symbol) AS quantity,
+      (SELECT SUM(quantity * price) FROM trades AS c WHERE c.symbol=a.symbol) AS costBasis
+    FROM trades AS a`,
     (err, data) => {
       if (err) { console.log(err); }
-      const key = Object.keys(data[0])[0];
-      console.log(data[0][key]);
       res.send(data);
     }
   );
@@ -57,81 +54,3 @@ app.get('/api/portfolio', (req, res) => {
 app.listen(1234, () => {
   console.log('Server is listening on port 3000');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Get all stocks in portfolio
-// TODO: Refactor to pull from trades table instead
-// app.get('/api/stocks', (req, res) => {
-//   db.query(`SELECT JSON_ARRAYAGG(JSON_OBJECT('symbol', s.symbol, 'quantity', s.quantity)) FROM stocks AS s`, (err, data) => {
-//     if (err) { console.log(err); }
-//     const key = Object.keys(data[0])[0];
-//     res.send(data[0][key]);
-//   });
-// });
-
-// // Get all stock symbols in portfolio
-// app.get('/api/stocks/symbols', (req, res) => {
-//   db.query(`SELECT JSON_ARRAYAGG(stockSymbol) FROM stocks`, (err, data) => {
-//     if (err) { console.log(err); }
-//     const key = Object.keys(data[0])[0];
-//     res.send(data[0][key]);
-//   });
-// });
-
-// app.get('/api/stocks/:stockSymbol/quantity', (req, res) => {
-//   const stockSymbol = req.params.stockSymbol;
-//   db.query(`SELECT quantity FROM stocks WHERE stockSymbol = ?`, [stockSymbol], (err, data) => {
-//     if (err) { console.log(err); }
-//     res.send(data[0]);
-//   });
-// });
-
-// app.get('/api/stocks/:stockSymbol/price', (req, res) => {
-//   const stockSymbol = req.params.stockSymbol;
-//   db.query(`SELECT price FROM stocks WHERE stockSymbol = ?`, [stockSymbol], (err, data) => {
-//     if (err) { console.log(err); }
-//     res.send(data[0]);
-//   });
-// });
-
-// Add new trade to trade history
-// TODO: Refactor this to post to trades table
-// app.post('/api/stocks', (req, res) => {
-//   const { quantity, stockSymbol, price } = req.body;
-//   db.query(`INSERT INTO stocks (quantity, stockSymbol, price) VALUES (?, ?, ?)`, [quantity, stockSymbol, price], (err, data) => {
-//     if (err) { console.log(err); }
-//     res.sendStatus(201);
-//   });
-// });
-
-// app.put('/api/stocks', (req, res) => {
-//   const { quantity, stockSymbol } = req.body;
-//   db.query(`UPDATE stocks SET quantity = ? WHERE stockSymbol = ?`, [quantity, stockSymbol], (err, data) => {
-//     if (err) { console.log(err); }
-//     res.send(data);
-//   });
-// });
-
-// app.delete('/api/stocks/:stockSymbol', (req, res) => {
-//   const { stockSymbol } = req.params;
-//   db.query(`DELETE FROM stocks WHERE stockSymbol = ?`, [stockSymbol], (err, data) => {
-//     if (err) { console.log(err); }
-//     res.send(data);
-//   });
-// });
